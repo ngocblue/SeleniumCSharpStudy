8f58717bbdbf29a430bb645b131aa1788e088c02
ngocblue <vuthibichngoc1989@gmail.com>
2022-08-30 17:25:41 +0700

    Initial commit unit combo control

---
+++ SeleniumCSharp_NetCore/UnitTest1.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumCSharp_NetCore
+{
+    public class Tests : DriverHelper
+    {
+
+
+        [SetUp]
+        public void Setup()
+        {
+            Driver = new ChromeDriver(@"D:\Work\Automation_Study\Driver");
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
+
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Almond");
+
+            Assert.Pass();
+        }
+    }
+}
